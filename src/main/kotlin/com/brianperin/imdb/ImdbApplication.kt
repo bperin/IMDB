5266f9bf4c1a4083fc3287927fc1045416d60986
Brian <bperin42@gmail.com>
2021-06-27 08:08:12 -0700

    enable some spring features

--- src/main/kotlin/com/brianperin/imdb/ImdbApplication.kt
+++ src/main/kotlin/com/brianperin/imdb/ImdbApplication.kt
@@ -2,8 +2,14 @@ package com.brianperin.imdb
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.boot.web.servlet.ServletComponentScan
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+import org.springframework.transaction.annotation.EnableTransactionManagement
 
 @SpringBootApplication
+@EnableTransactionManagement
+@EnableJpaAuditing
+@ServletComponentScan
 class ImdbApplication
 
 fun main(args: Array<String>) {
