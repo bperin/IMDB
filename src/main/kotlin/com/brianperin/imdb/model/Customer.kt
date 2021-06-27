0c2f7a60afb56beb5eed4677402c211556f75ee4
Brian <bperin42@gmail.com>
2021-06-27 10:53:29 -0700

    stub entity modeles for users / cinemas

---
+++ src/main/kotlin/com/brianperin/imdb/model/Customer.kt
@@ -0,0 +1,29 @@
+package com.brianperin.imdb.model
+
+import com.fasterxml.jackson.annotation.JsonProperty
+import jdk.jfr.Timestamp
+import org.hibernate.annotations.CreationTimestamp
+import org.hibernate.annotations.Type
+import org.hibernate.annotations.UpdateTimestamp
+import java.util.*
+import javax.persistence.*
+
+@Entity
+@Table(name = "customers")
+data class Customer(
+    @Id
+    @Type(type = "pg-uuid")
+    val id: UUID = UUID.randomUUID()
+) {
+    
+    @JsonProperty("created_at")
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false)
+    val createdAt: Date = Date()
+
+    @JsonProperty("updated_at")
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    val updatedAt: Date = Date()
+
+}
